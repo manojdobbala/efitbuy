9080a0ee886c8cf04ec655f55a02f5853306e205
Manoj Dobbala <manoj_k_dobbala@homedepot.com>
2018-07-05 00:06:35 -0500

    updated subheader

--- src/components/subheader.js
+++ src/components/subheader.js
@@ -17,7 +17,8 @@ import * as bannerImage from '../img/fitbay_cropped.png';
 import * as bannerImage1 from '../img/img_1.jpg';
 import * as bannerImage2 from '../img/img_2.jpg';
 import * as bannerImage3 from '../img/fitness.jpg';
-import * as bannerImage4 from '../img/img_4.jpg';
+import * as bannerImage4 from '../img/smoothies.jpg';
+import * as bannerImage5 from '../img/img_4.jpg';
 
 
 function TabContainer(props) {
@@ -67,6 +68,7 @@ class ScrollableTabsButtonForce extends React.Component {
                         <Tab label="Breakfast Recipes" icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM2 21h18v-2H2v2z"/></svg>} />
                         <Tab label="Lunch Recipes" icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z"/></svg>} />
                         <Tab label="Dinner Recipes" icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.1 13.34l2.83-2.83L3.91 3.5c-1.56 1.56-1.56 4.09 0 5.66l4.19 4.18zm6.78-1.81c1.53.71 3.68.21 5.27-1.38 1.91-1.91 2.28-4.65.81-6.12-1.46-1.46-4.2-1.1-6.12.81-1.59 1.59-2.09 3.74-1.38 5.27L3.7 19.87l1.41 1.41L12 14.41l6.88 6.88 1.41-1.41L13.41 13l1.47-1.47z"/></svg>} />
+                        <Tab label="Smoothie Recipes" icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 2l2.01 18.23C5.13 21.23 5.97 22 7 22h10c1.03 0 1.87-.77 1.99-1.77L21 2H3zm9 17c-1.66 0-3-1.34-3-3 0-2 3-5.4 3-5.4s3 3.4 3 5.4c0 1.66-1.34 3-3 3zm6.33-11H5.67l-.44-4h13.53l-.43 4z"/></svg>}/>
                         <Tab label="Fitness and Exercise" icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.57 14.86L22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29z"/></svg>} />
                     </Tabs>
                 </AppBar>
@@ -77,6 +79,7 @@ class ScrollableTabsButtonForce extends React.Component {
                 {value === 2 && <TabContainer><img src={bannerImage2} className="bannerImage"/></TabContainer>}
                 {value === 3 && <TabContainer><img src={bannerImage3} className="bannerImage"/></TabContainer>}
                 {value === 4 && <TabContainer><img src={bannerImage4} className="bannerImage"/></TabContainer>}
+                {value === 5 && <TabContainer><img src={bannerImage5} className="bannerImage"/></TabContainer>}
             </div>
         );
     }
